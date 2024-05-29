cf37935634fdbc6dbdae096df18af57f3106651e
George Soryal <79430767+GeorgeSoryal@users.noreply.github.com>
2024-05-29 05:36:49 -0400

    Added content

--- scripts.js
+++ scripts.js
@@ -1,9 +1,67 @@
 /* Place your JavaScript in this file */
-"use strict";
+
+class Item{
+    title;
+    content;
+    constructor(title, content){
+        this.title = title;
+        this.content = content;
+    }
+    
+    render(parentDiv) {
+        var header = document.createElement("h2");
+        header.innerHTML = this.title;
+        header.style.display = "inline";
+        header.style.background = "#A167A5";
+        header.style.padding = "0.5vw 0.5vw 0vw 0.5vw";
+        header.style.margin = "0vw";
+        var caption = document.createElement("p");
+        caption.style.display = "inline";
+        caption.style.background = "#5268A5";
+        caption.style.padding = "0.5vw 0.5vw 2vw 0.5vw";
+        caption.style.maxWidth = "35vw";
+        caption.style.margin = "0vw";
+        caption.innerHTML = this.content;
+        var item = document.createElement("div");
+        item.style.display = "grid";
+        document.getElementById(parentDiv).appendChild(item);
+        item.appendChild(header);
+        item.appendChild(caption);
+    }
+}
+
+class ItemCollection extends Array{
+    constructor(){
+        super(1);
+    }
+    add(item){
+        this.splice(0, 0, item);
+    }
+}
+
+function loadPage(){
+    fetch('navigation.txt')
+    .then(response => response.text())
+    .then(text => {
+            document.body.innerHTML = text;
+            sidebar = document.getElementById("sidebarDiv");
+            titleBar = document.getElementById("topDiv");
+            content = document.getElementById("content");
+            sidebarOpen = false;
+            sideBarAnimated = true;
+            var itemArray = new ItemCollection();
+            itemArray.add(new Item("New website up and running!", 
+            "Hello world! This is our website made to be ready for the 2024-2025 season of the FTC robotics competition!"));
+            itemArray.add(new Item("About us", "..."));
+            itemArray.forEach((x)=>{x.render("content")});}
+    )
+}
+
 var sidebar;
 var titleBar;
 var sidebarOpen;
 var sideBarAnimated;
+
 
 function openSideMenu(){
     sideBarAnimated = true;
@@ -23,6 +81,8 @@ function openSidebar(){
     sidebar.classList.remove("closeSidebar");
     sidebar.classList.add("openSidebar");
     titleBar.classList.add("shrinkTopbar");
+    content.classList.remove("growTopbar");
+    content.classList.add("shrinkTopbar");
     sidebarOpen = true;
 }
 
@@ -31,21 +91,11 @@ function closeSidebar(){
     sidebar.classList.remove("openSidebar");
     titleBar.classList.add("growTopbar");
     sidebar.classList.add("closeSidebar");
+    content.classList.remove("shrinkTopbar");
+    content.classList.add("growTopbar");
     sidebarOpen = false;
 }
 
-function loadPage(){
-    fetch('component.txt')
-    .then(response => response.text())
-    .then(text => {
-            document.body.innerHTML = text;
-            sidebar = document.getElementById("sidebarDiv");
-            titleBar = document.getElementById("topDiv");
-            sidebarOpen = false;
-            sideBarAnimated = true;
-        }
-    )
-}
 
 window.addEventListener("resize", function() {
     console.log("RESIZE")
@@ -64,3 +114,4 @@ window.addEventListener("resize", function() {
         titleBar.classList.add("staticTopbar")
     }
   })
+
