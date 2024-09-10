9680d477ae3fb23938de63d909f8a0963d55bc70
George Soryal <79430767+GeorgeSoryal@users.noreply.github.com>
2024-09-09 21:45:31 -0400

    Minified

--- sponsors/sponsors.js
+++ sponsors/sponsors.js
@@ -1,12 +1,1 @@
-"use strict";
-
-import * as navigation from "/navigation/navigation.js"
-
-new Promise(navigation.loadPage).then(content_div => {
-    document.getElementById("sponsors").classList.add("selected-link");
-
-    let sponsorUsDiv = document.createElement("div");
-    let thankSponsorsDiv = document.createElement("div");
-    content_div.appendChild(sponsorUsDiv);
-    content_div.appendChild(thankSponsorsDiv);
-});
+"use strict";import*as navigation from"/navigation/navigation.js";new Promise(navigation.loadPage).then((e=>{document.getElementById("sponsors").classList.add("selected-link");let n=document.createElement("div"),t=document.createElement("div");e.appendChild(n),e.appendChild(t)}));
